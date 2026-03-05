b7395ad77fae76992bb547e43edd1c2cdbf09df5
Uday Sharma <udaysharma.2310@gmail.com>
2026-03-05 19:51:27 +0530

    connected backend-to-frontend

--- 02-frontend-backend-connection/backend/server.js
+++ 02-frontend-backend-connection/backend/server.js
@@ -1,9 +1,34 @@
 import dotenv from 'dotenv';
 import express from'express';
+import cors from 'cors';
 const app= express();
 app.get('/',(req,res)=>{
     res.send('server is ready');
 });
+
+app.get('/api/jokes',(req,res)=>{
+    const jokes=[
+        {
+            id:1,
+            title:'A joke',
+            content:'This is a joke'
+        },
+        {
+            id:2,
+            title:'Another joke',
+            content:'This is another joke'
+        },
+        {
+            id:3,
+            title:'third joke',
+            content:'This is third joke'
+        }
+    ];
+    res.send(jokes);
+})
+
+
+
 dotenv.config()
 const port=process.env.PORT;
 app.listen(port,()=>{
---
+++ 02-frontend-backend-connection/frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import './App.css';
+import axios from 'axios'
+import { useEffect } from "react";
+
+function App(){
+ const [jokes,setjokes]=useState([])
+
+ useEffect(()=>{
+  axios.get('/api/jokes')
+  .then((response)=>{
+    setjokes(response.data)
+  })
+  .catch((error)=>{
+    console.log(error)
+  })
+ })
+
+ return(
+<>
+
+  <h1>HELLO WORLD</h1>
+  <p>Jokes: {jokes.length}</p>
+  {
+    jokes.map((joke,index)=>(
+      <div key={joke.id}>
+        <h3>{joke.title}</h3>
+        <p>{joke.content}</p>
+      </div>
+    ))
+  }
+
+</>
+)
+}
+
+export default App;
